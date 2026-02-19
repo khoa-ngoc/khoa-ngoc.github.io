a7d467f5049cd2762a49c92184339d882238ccb3
Do Duc Anh <doducanh48@gmail.com>
2026-02-19 15:54:36 +0700

    Fix autoplay

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@ let biicore = {
     'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/audio/BeautifulInWhite-ShaneFilan.mp3',
   alert: '[]',
   effect: { type: 'heart' },
-  isAutoPlay: false,
+  isAutoPlay: true,
 };
 
 var photoGalleries = [
